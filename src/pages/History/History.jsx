39e5fcbf88b2c9d79f8ee2768e5624cba1077fe4
aman-can <aman.dubey1998@gmail.com>
2022-04-28 18:12:14 +0530

    refactor: added page wrapper in the history page

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,44 +1,33 @@
-import { useHistorySevices } from "../../hooks";
+import { useHistorySevices, useWatchlaterSevices } from "../../hooks";
 import { HistoryVideoCard } from "./components/HistoryVideoCard/HistoryVideoCard";
 import styles from "./history.module.css";
 import { NothingToShow } from "../../components";
+import { PageWrapper } from "../components";
 
 export const History = () => {
     const { history, deleteVideoFromHistory, deleteAllVideosInHistory } =
         useHistorySevices(true);
-
+    const { watchlater, addToWatchlater } = useWatchlaterSevices(true);
     return (
-        <div className={`${styles["history-page"]}`}>
-            <div className={`${styles["history-header"]}`}>
-                <div>
-                    <p className="heading-5">History</p>
-                    <p className="text-body-lg">
-                        {!!history.length
-                            ? `${history.length} videos`
-                            : "No videos"}
-                    </p>
-                </div>
-                {!!history.length && (
-                    <button
-                        onClick={() => deleteAllVideosInHistory()}
-                        className="btn-filled-teal"
-                    >
-                        Clear
-                    </button>
-                )}
+        <PageWrapper
+            clearFunction={deleteAllVideosInHistory}
+            hearderText={"History"}
+            videoLength={history.length}
+            noClear
+        >
+            <div className={`${styles["histroy-card-layout"]}`}>
+                {history.map((video) => (
+                    <HistoryVideoCard
+                        isInWatchlater={watchlater.some(
+                            (item) => item._id === video._id
+                        )}
+                        handleAddWatchlater={addToWatchlater}
+                        key={video._id}
+                        videos={video}
+                        deleteVideo={deleteVideoFromHistory}
+                    />
+                ))}
             </div>
-            {!!history.length ? (
-                <div className={`${styles["histroy-card-layout"]}`}>
-                    {history.map((video) => (
-                        <HistoryVideoCard
-                            videos={video}
-                            deleteVideo={deleteVideoFromHistory}
-                        />
-                    ))}
-                </div>
-            ) : (
-                <NothingToShow />
-            )}
-        </div>
+        </PageWrapper>
     );
 };
